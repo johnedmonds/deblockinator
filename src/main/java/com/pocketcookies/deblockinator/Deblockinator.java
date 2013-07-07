bebc73acbade36c0ad1b5cba9da53dbb97fec88e
John Edmonds <john.a.edmonds@gmail.com>
2013-07-07 18:50:25 -0400

    Finish switching to BFS.

--- src/main/java/com/pocketcookies/deblockinator/Deblockinator.java
+++ src/main/java/com/pocketcookies/deblockinator/Deblockinator.java
@@ -16,38 +16,39 @@ public class Deblockinator {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+
     static List<Movement> getPathToEndState(BoardState startState) {
         return getPathToEndState(startState, Sets.<BoardState>newHashSet());
     }
-    
+
     private static class BFSScope {
+
+        private final BoardState boardState;
         private final Movement movement;
         private final List<Movement> previousMovements;
 
-        public BFSScope(Movement movement, List<Movement> previousMovements) {
+        public BFSScope(BoardState boardState, Movement movement, List<Movement> previousMovements) {
+            this.boardState = boardState;
             this.movement = movement;
             this.previousMovements = previousMovements;
         }
-
     }
-    
+
     private static List<Movement> getPathToEndState(BoardState startState, Set<BoardState> visitedBoardStates) {
         Queue<BFSScope> scopes = new LinkedList<BFSScope>();
         for (Movement movement : startState.getValidMovements()) {
-            scopes.add(new BFSScope(movement, ImmutableList.<Movement>of()));
+            scopes.add(new BFSScope(startState, movement, ImmutableList.<Movement>of()));
         }
         while (!scopes.isEmpty()) {
             BFSScope scope = scopes.poll();
-            BoardState boardState = startState.applyMovementToBlocks(scope.movement);
+            BoardState boardState = scope.boardState.applyMovementToBlocks(scope.movement);
             if (!visitedBoardStates.contains(boardState)) {
                 visitedBoardStates.add(boardState);
                 if (boardState.isEndState()) {
-                    return ImmutableList.<Movement>builder().add(scope.movement).addAll(scope.previousMovements).build();
+                    return ImmutableList.<Movement>builder().addAll(scope.previousMovements).add(scope.movement).build();
                 }
-                List<Movement> foundPath = getPathToEndState(boardState, visitedBoardStates);
-                if (foundPath != null) {
-                    return ImmutableList.<Movement>builder().add(scope.movement).addAll(foundPath).build();
+                for (Movement movement : boardState.getValidMovements()) {
+                    scopes.add(new BFSScope(boardState, movement, ImmutableList.<Movement>builder().addAll(scope.previousMovements).add(scope.movement).build()));
                 }
             }
         }
--- src/test/java/com/pocketcookies/deblockinator/DeblockinatorTest.java
+++ src/test/java/com/pocketcookies/deblockinator/DeblockinatorTest.java
@@ -18,4 +18,17 @@ public class DeblockinatorTest extends TestCase {
         List<Movement> actualMovements = Deblockinator.getPathToEndState(new BoardState(ImmutableList.of(new Block(0, 3, 2, Orientation.HORIZONTAL, true)), 6, 6, 5, 3));
         assertEquals(expectedMovements, actualMovements);
     }
+
+    public void testGetEndState_blocked() {
+        List<Movement> pathToEndState = Deblockinator.getPathToEndState(new BoardState(ImmutableList.of(
+                new Block(0, 3, 2, Orientation.HORIZONTAL, true),
+                new Block(4, 3, 2, Orientation.VERTICAL, false)), 6, 6, 5, 3));
+        assertEquals(ImmutableList.of(
+                new Movement(1, 0, new Block(0, 3, 2, Orientation.HORIZONTAL, true)),
+                new Movement(1, 0, new Block(1, 3, 2, Orientation.HORIZONTAL, true)),
+                new Movement(0, 1, new Block(4, 3, 2, Orientation.VERTICAL, false)),
+                new Movement(1, 0, new Block(2, 3, 2, Orientation.HORIZONTAL, true)),
+                new Movement(1, 0, new Block(3, 3, 2, Orientation.HORIZONTAL, true))),
+                pathToEndState);
+    }
 }
